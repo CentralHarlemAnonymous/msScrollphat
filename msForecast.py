d45a8c4a75c17b4ec3c8d1c8c404a6048b96392d
Michael Stern <nycstern+github@gmail.com>
2018-08-18 15:57:02 -0400

    new forecast now loads with each new call from outside

--- msForecast.py
+++ msForecast.py
@@ -3,27 +3,17 @@ import pyowm
 import json
 
 owm = pyowm.OWM('8473ffbb38cc053f91ecb29ae8ff9bf5')  # You MUST provide a valid API key
-# https://pyowm.readthedocs.io/en/latest/
-
-forecaster = owm.three_hours_forecast('Dobbs Ferry, US')
-forecast = forecaster.get_forecast()
-# weather_list = forecast.get_weathers()
-forecastJSON=json.loads(forecast.to_JSON())
-
-# for key in forecastJSON.keys():
-#    print key
 
 def forecasts(num):
-    outL = []
+    forecaster = owm.three_hours_forecast('Dobbs Ferry, US')
+    forecast = forecaster.get_forecast()
+    forecastJSON=json.loads(forecast.to_JSON())
+
     nForecasts = forecastJSON['weathers'][:num]
-    # for forecastDict in fourForecasts:
-    #     print forecastDict['detailed_status']
-    for n in nForecasts:
-       outL.append(n['detailed_status'])
-    return ''.join(outL)
+    return nForecasts
 
 def oneForecast():
-    mrForecast = forecastJSON['weathers'][:1]
+    mrForecast = forecasts(1)
     return mrForecast[0]['detailed_status']
 
 def printForecast():
